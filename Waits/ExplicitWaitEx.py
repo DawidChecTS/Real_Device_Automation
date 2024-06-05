532d573afcaa3e227f36766553336e79553ae765
Dawid_Chec <dawid.chec@testscouts.se>
2024-06-05 14:09:35 +0200

    Adding ExplictWaitEx file

--- Waits/ExplicitWaitEx.py
+++ Waits/ExplicitWaitEx.py
@@ -2,11 +2,7 @@ import time
 
 from appium.webdriver.common.appiumby import AppiumBy
 
-from Basic.LaunchApp import desired_caps, wait, driver
-from appium.options.android import UiAutomator2Options
-
-
-options = UiAutomator2Options().load_capabilities(desired_caps)
+from Basic.LaunchApp import wait, driver
 
 
 ele = wait.until(lambda x: x.find_element(AppiumBy.ID, "com.code2lead.kwad:id/EnterValue"))
@@ -17,4 +13,4 @@ ele.send_keys("Just Another Test.")
 
 time.sleep(2)
 
-driver.quit()
+#driver.quit()
