7076d271d0d570cd2368f5a754267f9a55c2b701
Dawid_Chec <dawid.chec@testscouts.se>
2024-06-05 12:53:10 +0200

    Updateing ByXpath file

--- locators/ByXpath.py
+++ locators/ByXpath.py
@@ -3,8 +3,18 @@ from appium.webdriver.common.appiumby import AppiumBy
 from Basic.LaunchApp import driver
 
 
-ele_xp = driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@content-desc="Btn5"]')
-ele_xp.click()
+#ele_xpath = driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@content-desc="Btn5"]')
+# - By using Xpath and Content Description
+#ele_xpath = driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@resource-id="com.code2lead.kwad:id/Zoom"]')
+# - By using Xpath and Resource-id
+#ele_xpath = driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@text="ZOOM"]')
+# - By using Xpath and text value
+ele_xpath = driver.find_element(AppiumBy.XPATH, '//android.widget.Button[@index="3"]')
+# - By using Xpath and index value
+ele_xpath.click()
+
+ele_class = driver.find_element(AppiumBy.XPATH, '//android.widget.EditText')
+ele_class.send_keys("Testing this Class xpath.")
 
 time.sleep(2)
 
