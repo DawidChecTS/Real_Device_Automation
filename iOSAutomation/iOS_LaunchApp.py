72afcdcc98a534c96cb1a1b262745c7d3d9b8785
Dawid_Chec <dawid.chec@testscouts.se>
2024-06-18 14:07:17 +0200

    Adiing iOS_LaunchApp file

---
+++ iOSAutomation/iOS_LaunchApp.py
@@ -0,0 +1,13 @@
+from appium import webdriver
+
+desired_caps = {}
+desired_caps['platformName'] = 'IOS'
+desired_caps['platformVersion'] = '13.2'
+desired_caps['deviceName'] = 'iPhone 11 Pro'
+desired_caps['automationName'] = 'XCUITest'
+desired_caps['app'] = ('')
+
+
+driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+
